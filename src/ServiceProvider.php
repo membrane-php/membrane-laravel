4efe6eed738df97a35817a3bb9e9122f81030262
John Charman <john.charman@imhotek.net>
2022-12-15 10:31:09 +0000

    ServiceProvider: merge default config values

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,14 +8,19 @@ use Membrane\Laravel\Middleware\RequestValidation;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    private const CONFIG_PATH = __DIR__ . '/../config/membrane.php';
+    private const CONFIG_NAME = 'membrane';
+
     public function boot(): void
     {
         /** @phpstan-ignore-next-line */ // config_path is a laravel framework helper method
-        $this->publishes([__DIR__ . '/../config/membrane.php' => config_path('membrane.php')], ['membrane']);
+        $this->publishes([self::CONFIG_PATH => config_path('membrane.php')], [self::CONFIG_NAME]);
     }
 
     public function register(): void
     {
+        $this->mergeConfigFrom(self::CONFIG_PATH, self::CONFIG_NAME);
+
         $this->app->when(RequestValidation::class)
             ->needs('$apiSpecPath')
             ->giveConfig('membrane.api_spec_file');
@@ -23,7 +28,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->app->when(ApiProblemBuilder::class)
             ->needs('$errorCode')
             ->giveConfig('membrane.validation_error_response_code');
-
         $this->app->when(ApiProblemBuilder::class)
             ->needs('$errorType')
             ->giveConfig('membrane.validation_error_response_type');
